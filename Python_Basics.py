3b38d3ebd34d7fd624ee0a9c995fccbc70c128c6
Ashish Saha <97810143+Ashish1100@users.noreply.github.com>
2023-01-19 19:49:05 +0530

    Update Python_Basics.py

--- Python_Basics.py
+++ Python_Basics.py
@@ -3,3 +3,24 @@
 #->denotes comments
 
 print("Hello, World!")
+
+
+
+#2 Loop:
+#Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+
+input("Enter the number", n);
+if n%2==1:
+    print("weird");
+elif n%2==0 & n>=2 & n<=5:
+    print("Not Weird");
+elif n%2==0 & n>=6 & n<=20:
+    print("Weird");
+elif n%2==0 & n>20:
+    print("Not Weird");
+    

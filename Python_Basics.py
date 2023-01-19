f2a71fecd75ecde097d3e2a8b29d96b54bbdf9c9
Ashish Saha <97810143+Ashish1100@users.noreply.github.com>
2023-01-19 19:51:52 +0530

    Update Python_Basics.py

--- Python_Basics.py
+++ Python_Basics.py
@@ -24,3 +24,17 @@ elif n%2==0 & n>=6 & n<=20:
 elif n%2==0 & n>20:
     print("Not Weird");
     
+
+#3. Arithmetic Operators
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers.
+        
+x=int(input())
+y=int(input())
+print(x+y)
+print(x-y)
+print(x*y)
+
+
